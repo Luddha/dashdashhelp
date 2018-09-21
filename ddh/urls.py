bc06a32a66ce08359f67b17f281df28efe10a394
Marvin S. Leister <mleister@aransaspasstx.net>
2018-09-21 16:03:06 -0500

    moved the banner of the header to a seperate include file banner.html, starting work on projects app

--- ddh/urls.py
+++ ddh/urls.py
@@ -19,7 +19,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 #    path('admin/', admin.site.urls),
-    path('', include('personal.urls'))
+    path('', include('personal.urls')),
+    path('projects/', include('projects.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
