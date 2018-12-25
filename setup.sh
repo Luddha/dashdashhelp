e87244d45a5a9b48653ece9e2f7f9f7eace41f6b
Marvin Leister <tuxbuddha@gmail.com>
2018-12-24 23:10:34 -0600

    setup file

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+WEBROOT="/var/www"
+APACHE_CONF="/etc/apache2/sites-available"
+
+
+if [[ $(id -u) -ne 0 ]]
+then
+	echo "must be root"
+	exit
+fi
+
+# install stuff
+echo "[installing packages]"
+apt-get install php7.2 apache2
+
+
+# move apache2 conf file and setup soft link
+echo "[moving conf file]"
+cp ./ddh.conf $APACHE_CONF/ddh.conf
+
+echo "[linking file to web root]"
+ln -s $(pwd)/ddh $WEBROOT/ddh
+
+echo "[enabling site]"
+a2dissite 000-default.conf
+a2ensite ddh
+service apache2 stop
+service apache2 start
